13e1c6df7b271b1b3cf04b90e6144e420f951347
Shota Teranishi <cattleya100519b@gmail.com>
2023-02-02 02:48:12 +0900

    230202: end ch01: make view(HelloWorld)

---
+++ tutorial/polls/urls.py
@@ -0,0 +1,5 @@
+# URLconf
+from django.urls import path
+from . import views
+
+urlpatterns = [path('', views.index, name='index'),]
--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    # root の URLconf に polls.urls module の記述を反映させる
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
